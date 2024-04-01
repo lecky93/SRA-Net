0f085e69baa9cb5bb3471ec74eea33cc63a85d66
lecky93 <108782949+lecky93@users.noreply.github.com>
2024-04-01 17:02:01 +0800

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,18 @@
+from .aumae import AuMAE
+from .vae import VAE
+from .weakly_net import WeaklyNet
+from .weakly_net2 import WeaklyNet2
+from .weakly_net3 import WeaklyNet3
+from .weakly_net4 import WeaklyNet4
+from .weakly_net5 import WeaklyNet5
+from .weakly_net6 import WeaklyNet6
+from .weakly_net7 import WeaklyNet7
+from .weakly_net8 import WeaklyNet8
+
+from .ssdl_net import SSDLNet
+
+from .resnet import ResNet_Uper
+from .deeplab import Deeplab_ASPP
+from .visionTrans import VisionTrans
+from .swin import Swin_Uper
+from .student import Student
